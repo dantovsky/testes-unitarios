098dd7da9ff02f56f774193dbda874f8a9b0ca1a
Dante Marinho <dantecnia@hotmail.com>
2021-06-27 20:29:37 +0100

    Aulas 16 e 17 - Verificações avançadas e Unindo jsonpath com Java.

--- Testando API REST/src/main/java/br/ce/wcaquino/rest/UserJsonTest.java
+++ Testando API REST/src/main/java/br/ce/wcaquino/rest/UserJsonTest.java
@@ -7,7 +7,9 @@ import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Locale;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
@@ -108,5 +110,49 @@ public class UserJsonTest {
         ;
     }
 
+    @Test // Aula 16 - Validações avançadas
+    public void deveFazerVerificacoesAvancadas() {
+        given()
+        .when()
+            .get("https://restapi.wcaquino.me/users")
+        .then()
+        .statusCode(200)
+            .body("$", hasSize(3))
+            .body("age.findAll{it <= 25}.size()", is(2)) // Quando users existem de até 25 anos? (it é a instância atual da idade)
+            .body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina")) // retorna lista
+            .body("findAll{it.age <= 25}[0].name", is("Maria Joaquina")) // retorna 1º registo de uma lista
+            .body("findAll{it.age <= 25}[-1].name", is("Ana Júlia")) // retorna último registo de uma lista
+            .body("find{it.age <= 25}.name", is("Maria Joaquina")) // retorna 1ª ocorrência
+            .body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+            .body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina"))
+            .body("name.collect{it.toUpperCase()}", hasItems("MARIA JOAQUINA")) // itera uma lista e faz transformacao em cima dela
+            .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItems("MARIA JOAQUINA"))
+            .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+            .body("age.collect{it * 2}", hasItems(60, 50, 40)) // multiplica cada idade por 2
+            .body("id.max()", is(3)) // qual o maior que existe na coleção
+            .body("salary.min()", is(1234.5678F)) // qual o menor salário
+            .body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678F, 0.001))) // soma dos salários da coleção
+            .body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000d), lessThan(5000d))) // soma dos salários da coleção
+        ;
+    }
 
+    @Test // Unindo jsonpath com Java
+    public void devoUnirJsonPathComJava() {
+
+        // Recebe uma lista de nomes
+        ArrayList<String> names =
+        given()
+        .when()
+            .get("https://restapi.wcaquino.me/users")
+        .then()
+            .statusCode(200)
+            .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+            .extract().path("name.findAll{it.startsWith('Maria')}")
+        ;
+
+        // Agora temos uma estrutura de "Java normal" para trabalhar
+        Assert.assertEquals(1, names.size()); // tamanho da lista é 1
+        Assert.assertTrue(names.get(0).equalsIgnoreCase("mArIa Joaquina"));
+        Assert.assertEquals(names.get(0).toUpperCase(), "maria joaquina".toUpperCase());
+    }
 }
